1c12af36ac953bca02a651e297e764e4c8880462
anonymous <68572566+kinsingo@users.noreply.github.com>
2026-04-23 01:48:20 +0900

    Update main.cpp

--- example/objectDetection/deepX-M1/main.cpp
+++ example/objectDetection/deepX-M1/main.cpp
@@ -1,16 +1,20 @@
+// Runtime Version v3.2.0
+
 #include "ai_bmt_gui_caller.h"
 #include "ai_bmt_interface.h"
 #include <filesystem>
 #include <opencv2/opencv.hpp>
 #include "dxrt/dxrt_api.h"
 #include "objectDetection_singleCore.cpp"
+#include "objectDetection_multiCore_wait.cpp"
 using namespace std;
 
 int main(int argc, char *argv[])
 {
     try
     {
-        shared_ptr<AI_BMT_Interface> interface = make_shared<ObjectDetection_Implementation_SingleCore>();
+        //shared_ptr<AI_BMT_Interface> interface = make_shared<ObjectDetection_Implementation_SingleCore>();
+        shared_ptr<AI_BMT_Interface> interface = make_shared<ObjectDetection_Implementation_MultiThreads>();
         return AI_BMT_GUI_CALLER::call_BMT_GUI_For_Single_Task(argc, argv, interface);
     }
     catch (const exception &ex)
